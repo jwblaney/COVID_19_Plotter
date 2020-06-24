06a4e1d062b81f3c9b9a37ecdf0e028fde1ee286
James Blaney <jw.blaney@gmail.com>
2020-06-24 15:53:51 -0600

    Rudimentary plotting and refrefreshing of data for selected state.

--- COVID-19 Plotter/Plotter.py
+++ COVID-19 Plotter/Plotter.py
@@ -5,22 +5,33 @@ import pandas as pd
 class Plotter(object):
     """description of class"""
 
-    def __init__(self, usdata = pd.DataFrame()):
-        self._USData = usdata
+    def __init__(self, data = pd.DataFrame()):
+        self._data = data
         self.y_vars = []
-        self.second_y_vars = []
 
-    def plotUSDaily(self):
+    def plotDailyCases(self):
         self.y_vars.append("positiveIncrease")
 
-    def plotTests(self):
+    def plotDailyTests(self):
         self.y_vars.append("totalTestResultsIncrease")
 
-    def plotPosRate(self):
+    def plotDailyPosRate(self):
         self.y_vars.append("percentPos")
 
-    def show(self):
-        self._USData.plot(x = "date", y =self.y_vars, subplots=True,  kind = "line")
+    def clearStats(self):
+        self.y_vars = []
+
+    def addDailyStats(self):
+        self.clearStats()
+        self.plotDailyCases()
+        self.plotDailyTests()
+        self.plotDailyPosRate()
+
+    def show(self, state):
+        plt.close()
+        self._data.plot(x = "date", y =self.y_vars, subplots=True,  kind = "line")
+        plt.title = state + " COVID-19 Statistics"
         plt.show()
 
-
+    def SetData(self, df):
+        self._data = df
